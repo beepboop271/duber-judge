ca674557f37e8ed59f88a2acf9062117afb3a830
Paula Yuan <ypaula521@gmail.com>
2021-01-16 16:49:20 -0500

    Fixed loop
    forgot about loop variables
    whoops

--- src/templater/Loop.java
+++ src/templater/Loop.java
@@ -2,12 +2,18 @@ package templater;
 
 public class Loop extends Node {
   private String target;
+  private String loopVariable;
 
-  public Loop(String target) {
+  public Loop(String target, String loopVariable) {
     this.target = target;
+    this.loopVariable = loopVariable;
   }
 
   public String getTarget() {
     return this.target;
+  }
+
+  public String getLoopVariable() {
+    return this.loopVariable;
   }
 }
