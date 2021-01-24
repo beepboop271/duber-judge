540d2c0ccbb91f8cf0b0b0154301fdce64ef55cf
Paula Yuan <ypaula521@gmail.com>
2021-01-24 14:54:58 -0500

    Update Interpreter.java

--- src/templater/Interpreter.java
+++ src/templater/Interpreter.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
+import templater.compiler.LanguageElement;
+
 public class Interpreter {
   private HashMap<String, Object> namespace;
 
@@ -11,21 +13,27 @@ public class Interpreter {
     this.namespace = namespace;
   }
 
-  public String resolveString(StringResolvable s) {
-    if (s.isTemplate()) {
-      Object content = this.namespace.get(s.getContent());
-      return content.toString();
+  public String resolveStrings(StringResolvables s) {
+    StringBuilder sb = new StringBuilder();
+    Iterator<StringResolvable> itr = s.iterator();
+    while (itr.hasNext()) {
+      StringResolvable toResolve = itr.next();
+      if (toResolve.isTemplate()) {
+        sb.append(this.namespace.get(toResolve.getContent()).toString());
+      } else {
+        sb.append(toResolve.getContent());
+      }
     }
-    return s.getContent();
+    return sb.toString();
   }
 
   public String interpret(Template syntaxTree) {
-    return interpretHelper(syntaxTree.getSyntaxTree(), "");
+    return interpretHelper(syntaxTree.getSyntaxTree(), new StringBuilder()).toString();
   }
 
-  public String interpretHelper(Node curNode, String interpreted) {
-    if (curNode instanceof Root) {
-      Iterator<Node> children = curNode.getChildren();
+  public StringBuilder interpretHelper(LanguageElement curElem, StringBuilder interpreted) {
+    if (curElem instanceof Root) {
+      Iterator<LanguageElement> children = ((Root)curElem).getChildren();
       // I mean it technically should only have one child so ashfdasf idk TODO
       while (children.hasNext()) {
         interpreted = interpretHelper(children.next(), interpreted);
@@ -33,22 +41,17 @@ public class Interpreter {
       return interpreted;
     }
 
-    if (curNode instanceof LiteralContent) {
-      return (interpreted += ((LiteralContent) curNode).getContent());
+    if (curElem instanceof StringResolvables) {
+      return (interpreted.append(resolveStrings((StringResolvables)curElem)));
     } 
 
-    if (curNode instanceof TemplatedContent) {
-      // TODO: what if it doesn't map to a string
-      return (interpreted += this.namespace.get(((TemplatedContent) curNode).getExpression()));
-    }
-    
-    if (curNode instanceof Loop) {
-      Loop loop = (Loop) curNode;
-      Object loopTarget = this.namespace.get(resolveString(loop.getTarget()));
+    if (curElem instanceof Loop) {
+      Loop loop = (Loop) curElem;
+      Object loopTarget = this.namespace.get(resolveStrings(loop.getTarget()));
       if (loopTarget instanceof Iterable<?>) {
         for (Object item : (Iterable<Object>)loopTarget) {
           this.namespace.put(loop.getLoopVariable(), item);
-          Iterator<Node> children = curNode.getChildren();
+          Iterator<LanguageElement> children = loop.getChildren();
           while (children.hasNext()) {
             interpreted = interpretHelper(children.next(), interpreted);
           }
@@ -59,47 +62,42 @@ public class Interpreter {
     }
 
     // only option left is element
-    Element elem = (Element) curNode;
-    interpreted += "<" + resolveString(elem.getName());
+    Element elem = (Element) curElem;
+    interpreted.append("<" + resolveStrings(elem.getName()));
     if (elem.getId() != null) {
-      interpreted += " id=" + resolveString(elem.getId());
+      interpreted.append(" id=" + resolveStrings(elem.getId()));
     }
 
-    Iterator<StringResolvable> classes = elem.getClasses();
+    Iterator<StringResolvables> classes = elem.getClasses();
     if (classes.hasNext()) {
-      interpreted += " class='";
+      interpreted.append(" class='");
       while (classes.hasNext()) {
-        interpreted += resolveString(classes.next())+" ";
+        interpreted.append(resolveStrings(classes.next())+" ");
       }
-      interpreted.trim();
-      interpreted += "'";
+      interpreted.append("'");
     }
 
-    Iterator<Map.Entry<StringResolvable, StringResolvable>> attributes =
+    Iterator<Map.Entry<String, StringResolvables>> attributes =
       elem.getAttributes();
     while (attributes.hasNext()) {
-      Map.Entry<StringResolvable, StringResolvable> attribute =
-        attributes.next();
-      interpreted +=
-        " "
-          +resolveString(attribute.getKey())
-          +"='"
-          +resolveString(attribute.getValue())
-          +"'";
+      Map.Entry<String, StringResolvables> attribute = attributes.next();
+      interpreted.append(
+        " "+attribute.getKey()+"='"+resolveStrings(attribute.getValue())+"'"
+      );
     }
 
     if (elem.isEmpty()) {
-      interpreted += "/>";
+      interpreted.append("/>");
       return interpreted;
     }
 
-    interpreted += ">";
+    interpreted.append(">");
 
-    Iterator<Node> children = curNode.getChildren();
+    Iterator<LanguageElement> children = elem.getChildren();
     while (children.hasNext()) {
       interpreted = interpretHelper(children.next(), interpreted);
     }
-    interpreted += "</"+resolveString(elem.getName())+">";
+    interpreted.append("</"+resolveStrings(elem.getName())+">");
     return interpreted;
   }
 }
